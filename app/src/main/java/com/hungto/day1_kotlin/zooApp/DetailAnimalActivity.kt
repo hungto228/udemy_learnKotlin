150c132cab3d00da7a30aeea9f97d017a21641a2
hungto228@gmail.com <hungto228@gmail.com>
2021-06-21 22:00:23 +0700

    animal demo

---
+++ app/src/main/java/com/hungto/day1_kotlin/zooApp/DetailAnimalActivity.kt
@@ -0,0 +1,11 @@
+package com.hungto.day1_kotlin.zooApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailAnimalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_animal)
+    }
+}
